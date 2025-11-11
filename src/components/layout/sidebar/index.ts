ac802033bddc78b5b42bf61e3896cae83304706f
SoySergo <polyakkovss@gmail.com>
2025-11-11 17:19:47 +0100

    - fix bag mobile menu

--- src/components/layout/sidebar/index.ts
+++ src/components/layout/sidebar/index.ts
@@ -4,9 +4,10 @@
  * показывает Desktop или Mobile версию в зависимости от размера экрана
  */
 
-export { Sidebar } from './Sidebar';
+export { Sidebar as DesktopSidebar } from './Sidebar';
 export { MobileSidebar } from './MobileSidebar';
 export { BottomNavigation } from './BottomNavigation';
 export { SidebarWrapper } from './SidebarWrapper';
+export { SidebarWrapper as Sidebar } from './SidebarWrapper'; // Дефолтный экспорт как Sidebar
 export { QueryItem } from './QueryItem';
 export { QueriesSelect } from './QueriesSelect';
